cd30b463364fc688ee06d7113e95ed86f04bc298
PureJoyMind <mazdak.parniandg@gmail.com>
2025-11-19 23:31:46 +0330

    generate a correct Directory.Packages.props file

--- CPMGen/Program.cs
+++ CPMGen/Program.cs
@@ -1,12 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using System.Text.RegularExpressions;
 using Buildalyzer;
 using Buildalyzer.Construction;
 
-var basePath = @"E:\VS Projects\CPMGen";
+var basePath = @"E:\VS Projects\Mentorly";
 
-    var solutionPath = Path.Combine(basePath, "CPMGen.sln");   
+    var solutionPath = Path.Combine(basePath, "Mentorly.sln");   
 if (File.Exists(solutionPath))
 {
     var solutionContent = File.ReadAllText(solutionPath);
@@ -22,18 +23,45 @@ if (File.Exists(solutionPath))
         var projectId = match.Groups[1].Value;
         var projectName = match.Groups[2].Value;
         var projectPath = match.Groups[3].Value;
-
+        if(!projectPath.EndsWith("csproj")) continue;
         Console.WriteLine($"Project Name: {projectName}, Project Path: {projectPath}");
         var manager = new AnalyzerManager();
         var analyzer = manager.GetProject(Path.Combine(basePath, projectPath));
         foreach (var reference in analyzer.ProjectFile.PackageReferences)
         {
             if(packages.TryGetValue(reference.Name, out var value)) value.Add(reference.Version);
+            else
+                packages.Add(reference.Name, new HashSet<string> { reference.Version });
         }
-        
-        
     }
+
+
+    var str = """
+                       <Project>
+                         <PropertyGroup>
+                           <ManagePackageVersionsCentrally>true</ManagePackageVersionsCentrally>
+                         </PropertyGroup>
+                         <ItemGroup>
+                       """;
+
+    var packageProps = new StringBuilder();
+    packageProps.AppendLine(str);
     
+    foreach (var kvp in packages)
+    {
+        foreach (var version in kvp.Value)
+            packageProps.AppendLine($"""    <PackageVersion Include="{kvp.Key}" Version="{version}" />""");
+    }
+
+    packageProps.AppendLine("""
+                              </ItemGroup>
+                            </Project>
+                            """);
+    
+    await using var fs = File.Create(Path.Combine(basePath, "Directory.Packages.props"));
+    await using var writer = new StreamWriter(fs);
+    await writer.WriteAsync(packageProps.ToString());
+    // await fs.WriteAsync;
 }
 else
 {
